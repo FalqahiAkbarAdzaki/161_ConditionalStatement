d85a04183fe058acc7e48e482782b49514454d64
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-03-11 11:41:36 +0700

    commit 2

--- file4.cpp
+++ file4.cpp
@@ -20,4 +20,12 @@ int main(){
     float Nilai1, Nilai2, Rerata;
     //Display "Masukkan Nilai1 = "
     cout << "Masukkan Nilai1 = ";
+    //Accept nNilai1
+    cin >> Nilai1;
+    //Display "Masukkan Nilai2 = "
+    cout << "Masukkan Nilai2 =";
+    //Accept nNilai2
+    cin >> Nilai2;
+    //Display "Status kelulusan = " + cStatus
+    cout << "Status kelulusan = " << fungsiCekStatusKelulusan(fungsiHitungRerata(Nilai1, Nilai2));
 }
